5f3b85dd6f93ad3b0c6e112058548d0a9f89de89
yagmurs <yagmur.sahin@comodo.com>
2019-02-05 16:11:41 +0300

    YS | New Solutions in Interview Preparation Kit

---
+++ InterviewPreparationKit/GreedyAlgorithms/LuckBalance/Solution.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the luckBalance function below.
+    static int luckBalance(int k, int[][] contests) {
+        int sum = 0;
+        ArrayList<Integer> sort = new ArrayList<>();
+        for (int i = 0; i < contests.length; i++){
+            sum += contests[i][0];
+            if (contests[i][1] == 1){
+                sort.add(contests[i][0]);
+            }
+        }
+        Collections.sort(sort);
+        int numLostImportant = sort.size() - k - 1;
+        while (numLostImportant > -1){
+            sum -= 2*sort.get(numLostImportant);
+            numLostImportant--;
+        }
+
+        return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[][] contests = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String[] contestsRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int contestsItem = Integer.parseInt(contestsRowItems[j]);
+                contests[i][j] = contestsItem;
+            }
+        }
+
+        int result = luckBalance(k, contests);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
