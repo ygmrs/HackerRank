920b612fe8007ce1b9abdd7c1790326e8f75fb4f
yagmurs <yagmur.sahin@comodo.com>
2019-02-20 14:33:50 +0300

    YS | New Solutions in Java

--- Java/JavaStringReverse/Solution.java
+++ Java/JavaStringReverse/Solution.java
@@ -58,5 +58,23 @@ public class Solution {
     }
 }
 
+//Another alternative solution is;
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A = sc.next();
+        for(int i=0; i<A.length()/2;i++){
+            if(A.charAt(i) != A.charAt(A.length()-i-1)){
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
 
